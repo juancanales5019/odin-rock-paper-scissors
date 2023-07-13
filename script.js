d9abdd3ba042b2c0a5b718af692f2e2dd3743718
Juan Pineda <juancanales5019@gmail.com>
2023-07-13 08:13:12 -0600

    git commit message "Recreate the whole game"

--- script.js
+++ script.js
@@ -1,75 +1,40 @@
-let userScore = 0;
-let computerScore = 0;
-const userScore_span = document.getElementById("userScore");
-const computerScore_span = document.getElementById("computerScore");
-const scoreboard_div = document.querySelector(".scoreboard");
-const result_p = document.querySelector(".result > p");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+var roundCount = 0;
+var playerScore = 0;
+var computerScore = 0;
 
-function convertToWord(letter) {
-    if (letter == "r") return "Rock";
-    if (letter == "p") return "Paper";
-    return "Scissors";
-}
+function play(userChoice) {
+    var choices = ['rock', 'paper', 'scissors'];
+    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-function wins(userChoise, computerChoise) {
-    userScore++;
-    userScore_span.innerHTML = userScore;
-    result_p.innerHTML = convertToWord(userChoise) + " beats " + convertToWord(computerChoise) + ", you win!";
-}
+    var result = '';
 
-function loses(userChoise, computerChoise) {
-    computerScore++;
-    computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = convertToWord(userChoise) + " beats " + convertToWord(computerChoise) + ", you lose!";
-}
-
-function draws(userChoise, computerChoise) {
-    result_p.innerHTML = "It is a draw";
-}
-
-function getComputerChoise() {
-    const choises = ["r", "p", "s"];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choises[randomNumber];
-}
-
-function game(userChoise) {
-    computerChoise = getComputerChoise();
-    switch (userChoise + computerChoise) {
-        case "rs":
-        case "sp":
-        case "pr":
-            wins(userChoise, computerChoise);
-            break;
-        case "rp":
-        case "ps":
-        case "sr":
-            loses(userChoise, computerChoise);
-            break;
-        case "rr":
-        case "pp":
-        case "ss":
-            draws(userChoise, computerChoise);
-            break;
+    if (userChoice === computerChoice) {
+        result = "It's a tie!";
+    } else if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        result = "You win!";
+        playerScore++;
+    } else {
+        result = "You lose!";
+        computerScore++;
     }
 
+    roundCount++;
+    document.getElementById('result').innerHTML = 'You chose ' + userChoice + '. The computer chose ' + computerChoice + '. ' + result;
+    document.getElementById('round-count').textContent = roundCount;
+    document.getElementById('player-score').textContent = playerScore;
+    document.getElementById('computer-score').textContent = computerScore;
 }
 
-function main() {
-    rock_div.addEventListener("click", function() {
-        game("r");
-    })
-
-    paper_div.addEventListener("click", function() {
-        game("p");
-    })
-
-    scissors_div.addEventListener("click", function() {
-        game("s");
-    })
+function reset() {
+    roundCount = 0;
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById('round-count').textContent = roundCount;
+    document.getElementById('player-score').textContent = playerScore;
+    document.getElementById('computer-score').textContent = computerScore;
+    document.getElementById('result').innerHTML = '';
 }
-
-main();
