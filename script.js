bdc857aee900203bfac73e5ae97cd64185d09862
Juan Pineda <juancanales5019@gmail.com>
2024-10-26 20:16:31 -0600

    Created a function to getHumanChoice

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("Hello world");
+console.log(getComputerChoice());
+console.log(getHumanChoice());
 //create a function to get computer choice
 function getComputerChoice() {
     //Use Math.random within the function to choose the return randomly
@@ -15,5 +17,23 @@ function getComputerChoice() {
         choice = "Scissors";
     }
     //the function has to return: "Rock", "Paper" or Scissors
+    return choice;
+}
+//Create a function to getHumanChoice
+function getHumanChoice() {
+    //create a variable to storage human choice
+    let choice = "";
+    //get Human choice using a prompt
+    //to simplify things human will choose 1 2 or 3
+    let number = parseInt(prompt("Choose 1 for Rock; 2 for Paper and 3 for Scissors"));
+    //convert human numeric choice into string: Rock Paper or Scissors
+    if (number == 1) {
+        choice = "Rock";
+    } else if (number == 2) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+    //Return human choice
     return choice;
 }
