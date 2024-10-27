efcfae98f923413545926d21e55a20714dc09067
Juan Pineda <juancanales5019@gmail.com>
2024-10-26 22:10:09 -0600

    Complete the task displaying result to the console
    Player now can play multiple times

--- script.js
+++ script.js
@@ -39,31 +39,57 @@ function getHumanChoice() {
 let computerScore = 0;
 //Declare humanScore variable
 let humanScore = 0;
+let draws = 0;
 
-//write the logic to play a single round
-//create a function that takes two arguments which are the computer and the human choices adn declare a winner
-function playRound(a,b) {
-    console.log(a);
-    console.log(b);
-    //use conditional to determine the winner in any give set of choices
-    //represent round winner for each possibility and log it to the console
-    if (a == b) {
-        console.log("it is a draw");
-    } else if ((a == "Rock" && b == "Scissors") || (a == "Paper" && b == "Rock") || (a == "Scissors" && b == "Paper")){
-        console.log("Computer Won");
-        computerScore += 1;
-        console.log(computerScore);
+//Create a new function named playGame
+function playGame(n) {
+    //call the playRoud function up to n number
+    for (let index = 1; index <= n; index++) {
+        //write the logic to play a single round
+        //create a function that takes two arguments which are the computer and the human choices adn declare a winner
+        function playRound(a,b) {
+             //console.log(a);
+             //console.log(b);
+            //use conditional to determine the winner in any give set of choices
+            //represent round winner for each possibility and log it to the console
+            if (a == b) {
+                console.log("it is a draw");
+                draws +=1;
+            } else if ((a == "Rock" && b == "Scissors") || (a == "Paper" && b == "Rock") || (a == "Scissors" && b == "Paper")){
+                console.log("Computer Won");
+                computerScore += 1;
+            } else {
+                console.log("You Won");
+                humanScore += 1;
+
+            }
+        }
+        //Declare variable to storage computer choice through the function getComputerChoice
+        let cChoice = getComputerChoice();
+        //Declare variable to storage human choice through the function getHumanChoice
+        let hChoice = getHumanChoice();
+        //Declare a variable to storage the result of calling the playRound function
+        let pround = playRound(cChoice,hChoice);
+        //console.log(pround);
+    }
+    if (humanScore > computerScore) {
+        console.log("Human won")
+        console.log(`Computer final Score: ${computerScore}`);
+        console.log(`Human final Score: ${humanScore}`);
+        console.log(`Draws total: ${draws}`);
+    } else if (humanScore < computerScore){
+        console.log("Computer won")
+        console.log(`Computer final Score: ${computerScore}`);
+        console.log(`Human final Score: ${humanScore}`);
+        console.log(`Draws total: ${draws}`);
     } else {
-        console.log("You Won");
-        humanScore += 1;
-        console.log(humanScore);
+        console.log("It is a draw")
+        console.log(`Computer final Score: ${computerScore}`);
+        console.log(`Human final Score: ${humanScore}`);
+        console.log(`Draws total: ${draws}`);
     }
 }
 
-//Declare variable to storage computer choice through the function getComputerChoice
-let cChoice = getComputerChoice();
-//Declare variable to storage human choice through the function getHumanChoice
-let hChoice = getHumanChoice();
-//Declare a variable to storage the result of calling the playRound function
-let pround = playRound(cChoice,hChoice);
-console.log(pround);
+//call the playGame function with any given number of rounds and store it into a variable
+let result = playGame(5);
+//console.log(result);
